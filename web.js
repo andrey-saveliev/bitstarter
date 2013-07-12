8a8d99f59b854cfb780a5b9d605d78404d34b464
Andrey S. Saveliev <Andrey.S.Saveliev@gmail.com>
2013-07-12 00:23:29 +0000

    Web.js from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs=require("fs");
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var htmlmsg=new Buffer(1024);
+  htmlmsg=fs.readFileSync("index.html");
+  response.send(htmlmsg.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
